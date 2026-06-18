7bda5a1a4505909e56529ca2ed34bcc25a420e36
iveyleeim981-cyber <iveyleeim981@gmail.com>
2026-06-18 06:00:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def fetch():
     'ignoreerrors': False,
     'extractor_args': {
         'youtube': {
-            'player_client': ['ios'],
+            'player_client': ['tv_embedded'],
         }
     },
 }
